a01bb8d62756a7907935249a9647cbefa3567b23
edend10 <edolev89@gmail.com>
2020-09-07 03:17:30 -0400

    comet

--- reqs.sh
+++ reqs.sh
@@ -3,6 +3,7 @@ pip install opencv-python
 pip install pyyaml
 pip install dill
 pip install numpy
+pip install comet_ml
 pip install progressbar2
 # python setup.py build_ext --inplace
 # python setup.py install --user
