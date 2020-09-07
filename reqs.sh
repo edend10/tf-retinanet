7c9d8d7b0597ae19f472a581f74351659b6519ef
edend10 <edolev89@gmail.com>
2020-09-07 03:13:41 -0400

    fixes

--- reqs.sh
+++ reqs.sh
@@ -3,6 +3,7 @@ pip install opencv-python
 pip install pyyaml
 pip install dill
 pip install numpy
+pip install progressbar2
 # python setup.py build_ext --inplace
 # python setup.py install --user
 
