f43bcbea53b7d292d42f79c5bddcf1b6761658c1
edend10 <edolev89@gmail.com>
2020-09-13 04:28:58 -0400

    reqs

--- reqs.sh
+++ reqs.sh
@@ -1,11 +1,26 @@
-pip install pillow
-pip install opencv-python
-pip install pyyaml
-pip install dill
-pip install numpy
-pip install comet_ml
-pip install progressbar2
-pip install tensorflow
-python setup.py build_ext --inplace
-python setup.py install --user
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -b
+
+ENV_NAME=tfr
+CONDA_CMD=~/miniconda3/bin/conda
+ENV_BIN_DIR=~/miniconda3/envs/${ENV_NAME}/bin
+PIP_CMD=${ENV_BIN_DIR}/pip
+PY_CMD=${ENV_BIN_DIR}/python
+
+$CONDA_CMD init
+
+$CONDA_CMD -n $ENV_NAME python=3.6 pip -y
+$CONDA_CMD install -n $ENV_NAME cudnn=7.6 -y
+
+$PIP_CMD install pillow
+$PIP_CMD install opencv-python
+$PIP_CMD install pyyaml
+$PIP_CMD install dill
+$PIP_CMD install numpy
+$PIP_CMD install comet_ml
+$PIP_CMD install progressbar2
+$PIP_CMD install tensorflow
+$PY_CMD setup.py build_ext --inplace
+
+$PY_CMD setup.py install --user
 
--- tf_retinanet/utils/defaults.py
+++ tf_retinanet/utils/defaults.py
@@ -34,11 +34,17 @@ default_callbacks_config = {
 
 
 default_anchors_config = {
-	'sizes'   : [32, 64, 128, 256, 512],
-	'strides' : [8, 16, 32, 64, 128],
-	'ratios'  : [0.5, 1, 2],
-	'scales'  : [2 ** 0, 2 ** (1.0 / 3.0), 2 ** (2.0 / 3.0)],
+	'sizes'   : [16, 32, 64, 128, 256],
+	'strides' : [4, 8, 16, 32, 64],
+        'ratios'  : [0.25, 1.0, 4.0],
+        'scales'  : [0.851, 1.036, 1.375],
 }
+#default_anchors_config = {
+#	'sizes'   : [32, 64, 128, 256, 512],
+#	'strides' : [8, 16, 32, 64, 128],
+#	'ratios'  : [0.5, 1, 2],
+#	'scales'  : [2 ** 0, 2 ** (1.0 / 3.0), 2 ** (2.0 / 3.0)],
+#}
 
 
 default_generator_config = {
